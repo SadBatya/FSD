c3d8844cf23df415e5815dd3840e03dced74b31d
sadbatya <sadbatya@mail.ru>
2026-01-06 15:03:38 +0200

    feat: footer

---
+++ src/shared/ui/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import style from "./Footer.module.css";
+
+export const Footer = () => (
+  <footer className={style.footer}>
+    <span>Политика конфиденциальности</span>
+    <span>Правила использования</span>
+    <span>2026 Все права защищены</span>
+  </footer>
+);
